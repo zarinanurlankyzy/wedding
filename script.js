867993cfbef4949a3b1a0839d793b8658a9852b1
zarinanurlankyzy <zarinanurlankyzy1@gmail.com>
2026-08-01 19:03:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,106 +6,80 @@ function sendAnswer(answer){
 
     let name = document.getElementById("guestName").value;
 
-
-    if(name.trim() === ""){
+    if(name.trim()==""){
 
         alert("Атыңызды жазыңыз 🤍");
-
         return;
 
     }
 
-
-    let url = 
-    "https://docs.google.com/forms/d/e/1FAIpQLSefdVfssikn6Xwv8GEgOVxNdrPP0BoK6_GeVzSO66DtGM8URQ/formResponse?"
-    +
-    "entry.539617390="
-    +
-    encodeURIComponent(name)
-    +
-    "&entry.180167126="
-    +
-    encodeURIComponent(answer);
-
-
+    let url =
+    "https://docs.google.com/forms/d/e/1FAIpQLSefdVfssikn6Xwv8GEgOVxNdrPP0BoK6_GeVzSO66DtGM8URQ/formResponse?" +
+    "entry.539617390=" + encodeURIComponent(name) +
+    "&entry.180167126=" + encodeURIComponent(answer);
 
     fetch(url,{
         method:"POST",
         mode:"no-cors"
     });
 
-
-
     alert("Рақмет 🤍\nЖауабыңыз қабылданды");
-
 
     document.getElementById("guestName").value="";
 
-}
-
-
-
-// ===============================
-// MUSIC
-// ===============================
-
-
-let music = new Audio("song.mp3");
-
-music.loop = true;
-
-let playing = false;
-
-
-
-function playMusic(){
-
-
-    let button = document.querySelector(".music-btn");
-
-
-    if(!playing){
-
-
-        music.play();
-
-
-        button.innerHTML="❚❚";
-
-
-        playing=true;
-
-
-    }
-
-    else{
-
-
-        music.pause();
-
-
-        button.innerHTML="♫";
-
-
-        playing=false;
-
-
-    }
+    document.querySelectorAll('input[name="answer"]').forEach(r=>r.checked=false);
 
 }
+
+// ===============================
+// КНОПКА ЖІБЕРУ
+// ===============================
 
 function submitSurvey(){
 
-    const selected=document.querySelector('input[name="answer"]:checked');
+    let selected=document.querySelector('input[name="answer"]:checked');
 
     if(!selected){
 
         alert("Жауапты таңдаңыз 🤍");
-
         return;
 
     }
 
     sendAnswer(selected.value);
+
+}
+
+// ===============================
+// MUSIC
+// ===============================
+
+let music=new Audio("song.mp3");
+
+music.loop=true;
+
+let playing=false;
+
+function playMusic(){
+
+    let button=document.querySelector(".music-btn");
+
+    if(!playing){
+
+        music.play();
+
+        button.innerHTML="❚❚";
+
+        playing=true;
+
+    }else{
+
+        music.pause();
+
+        button.innerHTML="♫";
+
+        playing=false;
+
+    }
 
 }
